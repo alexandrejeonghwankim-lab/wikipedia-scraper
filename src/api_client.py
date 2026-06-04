5f3c87aca1694218714779347ec186dd082ba364
Alex Kim <alexandre.jeonghwan.kim@gmail.com>
2026-06-04 15:14:04 +0200

    "API is ongoing"

--- src/api_client.py
+++ src/api_client.py
@@ -7,3 +7,22 @@
 # get_countries(): Queries the API and returns a clean list of supported country codes.
 # get_leaders(country: str): Fetches and returns the raw JSON list of leaders for a targeted country.
 
+class CountyLeadersAPI:
+    """Class responsible only for communicating with the REST API"""
+    def __init__(self, 
+                 base_url = "https://country-leaders.onrender.com", 
+                 country_endpoint, 
+                 leaders_endpoint, 
+                 cookies_endpoint, 
+                 session):
+        self.base_url = base_url
+        self.country_endpoint = 
+    
+    def refresh_cookie():
+        pass
+
+    def get_countries():
+        pass
+
+    def get_leaders(country: str):
+        pass
