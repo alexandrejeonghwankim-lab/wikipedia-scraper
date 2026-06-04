63b70415331f7880ad61cc0696a1a52b329a7fa5
Alex Kim <alexandre.jeonghwan.kim@gmail.com>
2026-06-04 17:28:58 +0200

    "API with description"

--- src/api_client.py
+++ src/api_client.py
@@ -10,9 +10,16 @@
 import requests
 
 class CountryLeadersAPI:
-    """Class responsible only for communicating with the REST API"""
+    """
+    This class handles all API communications such as retrieving countries,
+    leaders, and managing session cookie
+    """
     def __init__(self):
+        """Initialize the API client..
         
+        Sets the base URL, API endpoints, and creates a session
+        for making HTTP requests efficiently.
+        """    
         self.base_url = "https://country-leaders.onrender.com"
         self.country_endpoint = "/countries"
         self.leaders_endpoint = "/leaders"
@@ -20,11 +27,22 @@ class CountryLeadersAPI:
         self.session = requests.Session()
     
     def refresh_cookie(self):
+        """
+        Refresh the session cookies.
+
+        Sends a request to the cookie endpoint and returns the updated cookies.
+        """
         response = self.session.get(self.base_url + self.cookies_endpoint)
         return response.cookies
 
 
     def get_countries(self):
+        """
+        Retrieve the list of available country codes.
+
+        Returns:
+        list: A list of country codes from the API.
+        """
         url = self.base_url + self.country_endpoint
         cookies = self.refresh_cookie()
         response = self.session.get(url, cookies = cookies)
@@ -34,6 +52,15 @@ class CountryLeadersAPI:
         
 
     def get_leaders(self,country: str):
+        """
+        Retrieve leaders for a given country.
+
+        Args:
+        country (str): The country code.
+
+        Returns:
+        list: A list of leaders for the specified country.
+        """
         url = self.base_url + self.leaders_endpoint + "?country=" + str(country)
         cookies = self.refresh_cookie()
         response = self.session.get(url, cookies = cookies)
